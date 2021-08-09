7d4a48c6f4e8aadc17bd712a252a7bf1546c54ed
maria-a02 <maria.aramaki@gmail.com>
2021-08-08 23:36:33 -0400

    Routes and ufs_controller modified

--- app/controllers/api/v1/ufs_controller.rb
+++ app/controllers/api/v1/ufs_controller.rb
@@ -5,19 +5,8 @@ module Api
                 @client = Client.find_by(name: request.headers["X-CLIENTE"])
                 @client.login_count += 1
                 @client.save
-                @ufs = Uf.find_by(day: params[:date])
-                render json: jsoned_uf(@ufs)
-            end
-
-            private
-
-            def jsoned_uf(ufs)
-            array = []
-            h = {}
-            h['day'] = ufs.day.strftime("%Y/%m/%d")
-            h['value'] = ufs.value
-            array << h
-            return array
+                @ufs = Uf.find_by(day: params[:day])
+                render json: @ufs
             end
         end
     end
